b26e6d01fd32521c5ece977d04460e11fe108183
Reinaldo Junio Dias de Abreu <reinaldodiasabreu@gmail.com>
2021-12-08 11:22:46 -0300

    fix template-view-wide-right

--- campus/views.py
+++ campus/views.py
@@ -1,1 +1,14 @@
 from django.shortcuts import render
+
+
+def view_salas(request):
+    data = {}
+    data['projects'] = Project.objects.all()
+    form = ProjectForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_home')
+
+    data['form'] = form
+    return render(request, 'projects/home.html', data)
